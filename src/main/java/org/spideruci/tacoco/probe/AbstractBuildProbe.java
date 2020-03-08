606b99e2dcf18e5297e8bd3b42289554bda16d64
Vijay Krishna Palepu <vpalepu@uci.edu>
2020-03-07 18:58:39 -0800

    Making variables final; and other minor nits.

--- src/main/java/org/spideruci/tacoco/probe/AbstractBuildProbe.java
+++ src/main/java/org/spideruci/tacoco/probe/AbstractBuildProbe.java
@@ -8,30 +8,38 @@ public abstract class AbstractBuildProbe {
 	public static enum BuilderType {MAVEN, ANT, GRADLE, TBD};
 	private static AbstractBuildProbe instance = null;
 	
-	public final static AbstractBuildProbe getInstance(String AbsolutTargetPath){
-		//if(instance != null) return instance;
-		
-		switch(detectBuilder(AbsolutTargetPath)){
-		case MAVEN:
-			instance = new MavenBuildProbe(AbsolutTargetPath);
-			break;
-		case ANT:
-			instance = new AntBuildProbe(AbsolutTargetPath);
-			break;
-		case GRADLE:
-			instance = new GradleBuildProbe(AbsolutTargetPath);
-			break;
-		default:
-			break;
+	public final static AbstractBuildProbe getInstance(final String absoluteTargetPath) {
+
+		switch (detectBuilder(absoluteTargetPath)) {
+			case MAVEN:
+				instance = new MavenBuildProbe(absoluteTargetPath);
+				break;
+			case ANT:
+				instance = new AntBuildProbe(absoluteTargetPath);
+				break;
+			case GRADLE:
+				instance = new GradleBuildProbe(absoluteTargetPath);
+				break;
+			default:
+				break;
 		}
-		
+
 		return instance;
 	}
-	
-	private final static BuilderType detectBuilder(String AbsolutTargetPath){
-		if(new File(AbsolutTargetPath, "pom.xml").exists()) return BuilderType.MAVEN;
-		else if(new File(AbsolutTargetPath, "build.xml").exists()) return BuilderType.ANT;
-		else if(new File(AbsolutTargetPath, "build.gradle").exists()) return BuilderType.GRADLE;
+
+	private final static BuilderType detectBuilder(final String absoluteTargetPath) {
+		if (new File(absoluteTargetPath, "pom.xml").exists()) {
+			return BuilderType.MAVEN;
+		}
+		
+		if (new File(absoluteTargetPath, "build.xml").exists()) {
+			return BuilderType.ANT;
+		} 
+		
+		if (new File(absoluteTargetPath, "build.gradle").exists()) {
+			return BuilderType.GRADLE;
+		}
+
 		return BuilderType.TBD;
 	}
 	
